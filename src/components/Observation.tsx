33bde7a33daaa59f86e70b89c645edb1dc490c88
Kyle Karpack <kylekarpack@gmail.com>
2022-06-21 18:47:15 -0700

    Styling updates

--- src/components/Observation.tsx
+++ src/components/Observation.tsx
@@ -5,8 +5,13 @@ import styles from "./Observation.module.css";
 export const Observation: FunctionComponent<{ species: Taxon }> = ({ species }) => {
   return (
     <div className={styles.card}>
-      <div>image</div>
-      <div>{species.preferred_common_name}</div>
+      <div className={styles.photo} style={{ backgroundImage: `url("${species.default_photo?.medium_url}")` }}></div>
+      <div className={styles.text}>
+        <div>
+          <strong className={styles.title}>{species.preferred_common_name}</strong>
+        </div>
+        <div className={styles.subtitle}>({species.name})</div>
+      </div>
     </div>
   );
 };
--- src/components/Observations.tsx
+++ src/components/Observations.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent, useEffect, useState } from "react";
 import { INaturalistApiResponse, Taxon } from "../types/inaturalist";
 import { Observation } from "./Observation";
+import styles from "./Observations.module.css";
 
 export const Observations: FunctionComponent = () => {
   const [observedSpecies, setObservedSpecies] = useState<Taxon[]>([]);
@@ -8,7 +9,7 @@ export const Observations: FunctionComponent = () => {
   useEffect(() => {
     (async function () {
       const response = await fetch(
-        "https://api.inaturalist.org/v1/observations/species_counts?verifiable=any&user_id=kkarpack&locale=en-US&preferred_place_id=1&per_page=50"
+        "https://api.inaturalist.org/v1/observations/species_counts?verifiable=any&user_id=kkarpack&order_by=created_at&locale=en-US&preferred_place_id=1&per_page=50"
       );
       const data: INaturalistApiResponse = await response.json();
 
@@ -19,9 +20,11 @@ export const Observations: FunctionComponent = () => {
   return (
     <>
       Recently Observed Species:{" "}
-      {observedSpecies.map((el) => {
-        return <Observation key={el.id} species={el} />;
-      })}
+      <div className={styles.observations}>
+        {observedSpecies.map((el) => {
+          return <Observation key={el.id} species={el} />;
+        })}
+      </div>
     </>
   );
 };
