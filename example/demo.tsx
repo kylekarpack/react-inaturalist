479471773f7e2538c2a0807ac9d79b06626893fa
Kyle Karpack <kylekarpack@gmail.com>
2022-12-17 13:35:50 -0800

    Basic Parcel addition

--- example/demo.tsx
+++ example/demo.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Observations } from "../src/index";
 
 const container = document.getElementById("home");
-const root = createRoot(container);
+const root = createRoot(container!);
 
 root.render(
   <div>
