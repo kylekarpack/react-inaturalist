a6928747fcc6844d12c0f1a3a8abef3a1406d365
Kyle Karpack <kylekarpack@gmail.com>
2023-06-03 16:18:34 -0700

    Add dark mode

--- example/demo.tsx
+++ example/demo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Observations } from "../src/index";
+import "./demo.css";
 
 const container = document.getElementById("home");
 const root = createRoot(container!);
@@ -8,6 +9,6 @@ const root = createRoot(container!);
 root.render(
   <div>
     <h1>React iNaturalist Demo Page</h1>
-    <Observations user_id={["kkarpack"]} per_page={30} />
+    <Observations user_id={["kkarpack"]} per_page={50} />
   </div>
 );
